9db28fa1c631d726314c3e31828d0c2ad1cb570b
Janhavi-Khairnar <khairnarj2000@gmail.com>
2023-04-16 12:27:26 +0530

    [ADDED]:[Janhavi] Set Initial Position of Player

---
+++ src/com/technospace/snakeandladder/SnakeLadderMain.java
@@ -0,0 +1,19 @@
+package com.technospace.snakeandladder;
+
+public class SnakeLadderMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/***
+		 * Using this variable sets the initial position of a player.
+		 */
+		
+		int position = 0;
+		
+		System.out.println("Initial Position of a Player :"+position);
+		
+		
+	}
+
+}
