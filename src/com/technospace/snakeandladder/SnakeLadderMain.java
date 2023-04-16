68112bf6a86d738edd00907f78e11b935da81c45
Janhavi-Khairnar <khairnarj2000@gmail.com>
2023-04-16 12:48:27 +0530

    [MODIFIED]:[Janhavi] Using Die set New Position

--- src/com/technospace/snakeandladder/SnakeLadderMain.java
+++ src/com/technospace/snakeandladder/SnakeLadderMain.java
@@ -1,18 +1,29 @@
 package com.technospace.snakeandladder;
 
+import java.util.Random;
+
 public class SnakeLadderMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		/***
+		 * Using Object of Random Class to generate the die number between 1-6.
+		 */
+		Random random = new Random();
+		
+		/***
 		 * Using this variable sets the initial position of a player.
 		 */
-		
 		int position = 0;
-		
 		System.out.println("Initial Position of a Player :"+position);
 		
+		
+		int dieNo = random.nextInt(6)+1;
+		System.out.println("Die Number :"+dieNo);
+		
+		position = position + dieNo;
+		System.out.println("New Position after Rolling the Die :"+position);
 		
 	}
 
