b9552e9a29a466abe9515e07f5943e8e4f0e24a5
Janhavi-Khairnar <khairnarj2000@gmail.com>
2023-04-16 14:05:40 +0530

    [MODIFIED]:[Janhavi] Using Switch Case Select Option

--- src/com/technospace/snakeandladder/SnakeLadderMain.java
+++ src/com/technospace/snakeandladder/SnakeLadderMain.java
@@ -7,6 +7,11 @@ public class SnakeLadderMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int position = 0;
+		final int NO_PLAY = 1;
+		final int LADDER = 2;
+		final int	SNAKE = 3;
+		
 		/***
 		 * Using Object of Random Class to generate the die number between 1-6.
 		 */
@@ -15,16 +20,41 @@ public class SnakeLadderMain {
 		/***
 		 * Using this variable sets the initial position of a player.
 		 */
-		int position = 0;
-		System.out.println("Initial Position of a Player :"+position);
+		
+			System.out.println("Initial Position of a Player :"+position);
 		
 		
-		int dieNo = random.nextInt(6)+1;
-		System.out.println("Die Number :"+dieNo);
+			int dieNo = random.nextInt(6)+1;
+			System.out.println("Die Number :"+dieNo);
+			
+			position = position + dieNo;
+			
+			int option = random.nextInt(3)+1;
+			System.out.println("Option Number :"+option);
+			switch(option)
+			{
+				case NO_PLAY:
+					System.out.println("Option = No Play");
+					position = position;
+					break;
+				case LADDER:
+					System.out.println("Option = Ladder");
+					position = position + dieNo;
+					break;
+				case SNAKE:
+					System.out.println("Option = Snake");
+					position = position - dieNo;
+					if(position < 0)
+					{
+						position = 0;
+					}
+					break;
+			}
+			
+			System.out.println("New Position :"+position);
 		
-		position = position + dieNo;
-		System.out.println("New Position after Rolling the Die :"+position);
 		
 	}
+		
 
 }
