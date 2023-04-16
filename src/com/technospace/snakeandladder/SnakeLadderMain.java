2a270d3a47757236c7092573b408a3bbca1e159d
Janhavi-Khairnar <khairnarj2000@gmail.com>
2023-04-16 14:42:34 +0530

    [MODIFIED]:[Janhavi] Repeat the Game till to reach the Winning Position

--- src/com/technospace/snakeandladder/SnakeLadderMain.java
+++ src/com/technospace/snakeandladder/SnakeLadderMain.java
@@ -6,44 +6,41 @@ public class SnakeLadderMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		/***
+		 * Using this variable sets the initial position of a player.
+		 */
 		int position = 0;
 		final int NO_PLAY = 1;
 		final int LADDER = 2;
-		final int	SNAKE = 3;
+		final int SNAKE = 3;
 		
 		/***
 		 * Using Object of Random Class to generate the die number between 1-6.
 		 */
 		Random random = new Random();
 		
-		/***
-		 * Using this variable sets the initial position of a player.
-		 */
+		System.out.println("Initial Position of a Player :"+position);
 		
-			System.out.println("Initial Position of a Player :"+position);
-		
-		
+		while(position != 100)
+		{	
 			int dieNo = random.nextInt(6)+1;
 			System.out.println("Die Number :"+dieNo);
-			
-			position = position + dieNo;
 			
 			int option = random.nextInt(3)+1;
 			System.out.println("Option Number :"+option);
 			switch(option)
 			{
 				case NO_PLAY:
-					System.out.println("Option = No Play");
-					position = position;
+						System.out.println("Option = No Play");
+						position = position;
 					break;
 				case LADDER:
-					System.out.println("Option = Ladder");
-					position = position + dieNo;
+						System.out.println("Option = Ladder");
+						position = position + dieNo;
 					break;
 				case SNAKE:
-					System.out.println("Option = Snake");
-					position = position - dieNo;
+						System.out.println("Option = Snake");
+						position = position - dieNo;
 					if(position < 0)
 					{
 						position = 0;
@@ -52,9 +49,9 @@ public class SnakeLadderMain {
 			}
 			
 			System.out.println("New Position :"+position);
-		
+			System.out.println("#######");
 		
 	}
-		
+	}	
 
 }
